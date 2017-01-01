0d2d10c1bd808d4802f0e261e92c6470d24ef86d
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2017-01-01 23:00:00 +0900

    Changed lint rule and fixed some warns

--- extension/src/util/colors.ts
+++ extension/src/util/colors.ts
@@ -1,5 +1,5 @@
 export namespace Colors {
     export function isTransparentCode(code: string): boolean {
-        return code === "#00000000" || code == "#0000000";
+        return code === "#00000000" || code === "#0000000";
     }
 }
