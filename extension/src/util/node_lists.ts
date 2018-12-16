48a936e930794aa17b23257fae46b45bef664b8c
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-12-16 18:33:25 +0900

    Addressed lint

--- extension/src/util/node_lists.ts
+++ extension/src/util/node_lists.ts
@@ -2,8 +2,8 @@ import { Nodes } from "./nodes";
 
 export namespace NodeLists {
     export function findVectorNode(nodes: NodeList): Element {
-        const vectorNodes: Array<Element> = [];
-        nodes.forEach(node => {
+        const vectorNodes: Element[] = [];
+        nodes.forEach((node) => {
             if (Nodes.isVector(node)) {
                 vectorNodes.push(node as Element);
             } else if (!Nodes.isComment(node)) {
