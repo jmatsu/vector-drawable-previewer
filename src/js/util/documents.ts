8a5e1d8510499089e53001485e2774806e2cdc92
Jumpei Matsuda <jmatsu.drm@gmail.com>
2020-05-31 03:46:46 +0900

    yarn upgrade all

--- src/js/util/documents.ts
+++ src/js/util/documents.ts
@@ -5,26 +5,30 @@ export namespace Documents {
         return (document.body || document).childNodes;
     }
 
-    export function createPreviewElement(svg: Node): Element {
+    export function createPreviewElement(svg: SVGElement): Element {
         const root = document.createElement("div");
-        root.setAttribute("style", "padding: 10px; display: flex; flex-direction: row; align-items: center;");
+        root.style.padding = "10px";
+        root.style.display = "flex";
+        root.style.flexDirection = "row";
+        root.style.alignItems = "center";
 
         const svgContainer = document.createElement("div");
-        svgContainer.setAttribute("id", Id.svgContainer);
-        svgContainer.setAttribute("style", "margin: 10px;");
+        svgContainer.id = Id.svgContainer
+        svgContainer.style.margin = "10px";
         svgContainer.appendChild(svg);
 
         const optContainer = document.createElement("div");
-        optContainer.setAttribute("style", "display: inline-block;");
+        optContainer.style.display = "inline-block";
 
-        const colorText = document.createElement("input");
-        colorText.type = "color"
-        colorText.onchange = (e) => {
+        const bgColorInput = document.createElement("input");
+        bgColorInput.id = Id.svgColorElement
+        bgColorInput.type = "color"
+        bgColorInput.onchange = (e) => {
             const target = e.target as HTMLInputElement;
-            (svg as Element).setAttribute("style", `background-color: ${target.value};`);
+            (svg as SVGElement).style.backgroundColor = target.value;
         }
 
-        optContainer.appendChild(colorText);
+        optContainer.appendChild(bgColorInput);
 
         root.appendChild(svgContainer);
         root.appendChild(optContainer);
