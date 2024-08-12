8498baf81af6cf750a3bee03b4f6a700b09f8d42
Jumpei Matsuda <jmatsu.drm@gmail.com>
2024-08-12 10:14:23 +0900

    chore(webpack): Use module instead cjs

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,41 +1,49 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = "../src/js/";
+import { fileURLToPath } from "url";
+import CopyPlugin from "copy-webpack-plugin";
+import webpack from "webpack";
+import path from "path";
 
-module.exports = {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __project_dirname = path.resolve(__dirname, "../");
+
+export default {
   entry: {
-    background: path.join(__dirname, srcDir + "background.ts"),
-    content_script: path.join(__dirname, srcDir + "content_script.ts")
+    background: path.join(__project_dirname, "src", "js", "background.ts"),
+    content_script: path.join(
+      __project_dirname,
+      "src",
+      "js",
+      "content_script.ts",
+    ),
   },
   output: {
-    path: path.join(__dirname, "../dist/js"),
-    filename: "[name].js"
+    path: path.join(__project_dirname, "dist", "js"),
+    filename: "[name].js",
   },
   optimization: {
     splitChunks: {
       name: "vendor",
-      chunks: "initial"
-    }
+      chunks: "initial",
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
-         contentBase: './dist',
-      },
+    contentBase: "./dist",
+  },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js"],
   },
   plugins: [
     // exclude locale files in moment
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new CopyPlugin([{ from: ".", to: "../" }], { context: "public" })
-  ]
+    new CopyPlugin([{ from: ".", to: "../" }], { context: "public" }),
+  ],
 };
